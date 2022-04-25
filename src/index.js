3c4ca7cb465169aff8d0a7502b0d52f7f719731d
ConiVC96 <constanzavc.96@outlook.cl>
2022-04-25 18:44:08 -0300

    Ok

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
 import validator from './validator.js';
 
 
-// declaración de variables
 const form = document.querySelector('#paymentForm');
-const inputList = Array.from(document.querySelectorAll('input'));  // [...]
+const inputList = Array.from(document.querySelectorAll('input'));  
 const inputPayment = document.getElementById('creditCard');
-let paymentTemporalvalue = ''; //inicializo la variable
+let paymentTemporalvalue = ''; 
 
-// eventListener onchange (cuando el valor del input cambie), recibe el nombre y una función
+
 inputPayment.addEventListener('change', (e) => {
-  let value = e.target.value                       // esto es lo mismo que poner inputPayment.value pero por convención es mejor este
-  let maskData = validator.maskify(value)         // esta variable retorna el valor de maskify, que es el input enmascarado 
-  paymentTemporalvalue = inputPayment.value      // a la variable pTV le pasaré el valor de iP para guardar el valor antes de transformarse 
- //para después cambiar el valor al Maskdata
+  let value = e.target.value                      
+  let maskData = validator.maskify(value)         
+  paymentTemporalvalue = inputPayment.value      
   inputPayment.value = maskData   
 })
 
-// eventListener onsubmit (cuando el formulario se ha enviado)
+
 form.addEventListener('submit', (e) => {
-  e.preventDefault()                         // por defecto actualiza la página, con esto evitamos ese comportamiento
-  inputList.forEach( function(input) {        //Por cada uno de los input voy a preguntar si están vacíos
+  e.preventDefault()                         
+  inputList.forEach( function(input) {        
     if(input.value === ''){
-      alert(input.placeholder + ' es obligatorio') //${}
+      alert(input.placeholder + ' es obligatorio') 
     }
   })
   validator.isValid(paymentTemporalvalue);
--- src/validator.js
+++ src/validator.js
@@ -2,21 +2,16 @@ const validator = {
   isValid : function (creditNumber) { 
    let splitNumbers = creditNumber.split('')
    let reverseNumbers = splitNumbers.reverse()
-   let digits = reverseNumbers.map(Number); // crea un nuevo array de números, le da reversa, los divide con , y los guarda en digitos
+   let digits = reverseNumbers.map(Number); 
    
-    for (let i = 0; i < digits.length; i++) { //inicia la iteración (i) en 0, el largo de digitos debe ser menor a i, y en cada iteración se le suma 1
-    432
+    for (let i = 0; i < digits.length; i++) { 
       if (i % 2 ) { 
-        digits[i] *= 2; //digits[i] = digits[i] * 2
+        digits[i] *= 2; 
         if (digits[i] > 9) {
           digits[i] -= 9;
-        }    // este if hace un modulo de la posición, siendo 0 falso y 1 verdadero, de ser verdadero *2 el valor de la posición de 2
-      }    // el segundo if verifica
+        }    
+      }   
     } 
-
-    /* let sum = eval(digits.join('+')); //eval convierte a números para así poder sumarlo con el join parseint*** 
-    console.log(typeof(digits)) */
-
     let sum = 0; 
 
       for (let i = 0; i < digits.length; i++)  {
@@ -27,11 +22,8 @@ const validator = {
   } 
     return false
   }, 
-
   maskify: function (inputCard) {
-    
-    return inputCard.slice(0,-4).replace(/./g, "#") + inputCard.slice(-4); // separa los digitos, los reemplaza y le agrega los últimos 4
-
+    return inputCard.slice(0,-4).replace(/./g, "#") + inputCard.slice(-4); 
   }
 }
   export default validator; 
